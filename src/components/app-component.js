bdd9a935520c83f35595e4f793a10fd48b109078
sota1235 <sota1235@gmail.com>
2016-04-25 01:13:01 +0900

    modify: hello world by react.js

---
+++ src/components/app-component.js
@@ -0,0 +1,14 @@
+/**
+ * @description Main component.
+ */
+import React from 'react';
+
+export default class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <p>Hello, Kakitama!</p>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * @description For index page.
+ */
+import React        from 'react';
+import { render }   from 'react-dom';
+import AppComponent from './components/app-component';
+
+render(
+  <AppComponent />,
+  document.getElementById('app-mount-space')
+);
