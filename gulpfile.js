e0c508fe5664a5913411aacf31e13bf801cbc88b
sota1235 <sota1235@gmail.com>
2016-04-26 01:39:47 +0900

    modify: add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
-const gulp  = require('gulp');
-const babel = require('gulp-babel');
+"use strict";
 
+const gulp        = require('gulp');
+const babel       = require('gulp-babel');
+const del         = require('del');
+const runSequence = require('run-sequence');
+
+// clean build files
+gulp.task('clean', () => {
+  del('./app');
+});
+
+// export node_modules files
+gulp.task('export-node-modules', () => {
+  const exportModules = [
+    'font-awesome'
+  ];
+
+  for(let exportModule of exportModules) {
+    gulp.src(`node_modules/${exportModule}/**/*`)
+      .pipe(gulp.dest('app/node_modules'));
+  }
+});
+
+// export html files
 gulp.task('export-html', () => {
   return gulp
     .src('src/**/*.html')
     .pipe(gulp.dest('app'));
 });
 
+// compile js written in ES2015
 gulp.task('es6', () => {
   return gulp
     .src('src/**/*.js')
@@ -21,4 +44,7 @@ gulp.task('es6', () => {
     .pipe(gulp.dest('app'))
 });
 
-gulp.task('build', ['es6', 'export-html']);
+// default build task
+gulp.task('build', (cb) => {
+  return runSequence('clean', 'es6', 'export-html', 'export-node-modules', cb);
+});
