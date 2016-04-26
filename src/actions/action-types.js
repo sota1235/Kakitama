5ec59dde8590c54fe8a34af2ee6caf29f354c85a
sota1235 <sota1235@gmail.com>
2016-04-27 01:55:43 +0900

    modify: add action name and actions

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -2,4 +2,30 @@
  * @description Action types for app.
  */
 
+/**
+ * @description Adding todo.
+ * @type {string}
+ */
 export const ADD_TODO = 'ADD_TODO';
+
+/**
+ * @description Complete todo.
+ * @type {string}
+ */
+export const COMPLETE_TODO = 'COMPLETE_TOD';
+
+/**
+ * @description Visible todo.
+ * @type {string}
+ */
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+/**
+ * @description Constants for visibility status.
+ * @type {Object}
+ */
+export const VisibilityFilters = {
+  SHOW_ALL       : 'SHOW_ALL',
+  SHOW_COMPLETED : 'SHOW_COMPLETED',
+  SHOW_ACTIVE    : 'SHOW_ACTIVE'
+}
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -12,3 +12,21 @@ import * as ActionNames from './action-types';
 export function addTodo(text) {
   return { type: ActionNames.ADD_TODO, text };
 }
+
+/**
+ * @description Dispatch completing TODO action.
+ * @param {number} index
+ * @return {Object}
+ */
+export function completeTod(index) {
+  return { type: ActionNames.COMPLETE_TODO, index };
+}
+
+/**
+ * @description Dispatch setting visibility status action.
+ * @param {string} filter
+ * @return {Object}
+ */
+export function setVisibiltyFilter(filter) {
+  return { type: ActionNames.SET_VISIBILITY_FILTER, filter };
+}
