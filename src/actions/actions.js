4f77d7a7a063f15bc622815a2d4d180a3c112f59
sota1235 <sota1235@gmail.com>
2016-04-30 02:01:04 +0900

    modify: add action methdo for redux tutorial app

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,13 +4,20 @@
 
 import * as ActionNames from './action-types';
 
+/** @type {number} auto increment number for todo list. */
+let nextTodoId = 0;
+
 /**
  * @description Dispatch adding TODO action.
  * @param {string} text
  * @return {Object}
  */
 export function addTodo(text) {
-  return { type: ActionNames.ADD_TODO, text };
+  return {
+    type: ActionNames.ADD_TODO,
+    id  : nextTodoId++,
+    text
+  };
 }
 
 /**
@@ -29,4 +36,13 @@ export function completeTodo(index) {
  */
 export function setVisibilityFilter(filter) {
   return { type: ActionNames.SET_VISIBILITY_FILTER, filter };
+}
+
+/**
+ * @description Toggle todo status.
+ * @param {number} id
+ * @return {Object}
+ */
+export const toggleTodo = (id) => {
+  return { type: ActionNames.TOGGLE_TODO, id };
 }
