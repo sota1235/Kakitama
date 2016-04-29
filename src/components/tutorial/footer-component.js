5611ef9e9353bb6582b7aaca974d9be8f2d93235
sota1235 <sota1235@gmail.com>
2016-04-30 02:11:53 +0900

    modify: fix components(for Redux tutorial)

--- src/components/tutorial/footer-component.js
+++ src/components/tutorial/footer-component.js
@@ -2,16 +2,21 @@
  * @description Footer component.
  */
 
-import React from 'react';
-import FilterLink from '../../containers/filter-link-container';
+import React                 from 'react';
+import FilterLink            from '../../containers/filter-link-container';
+import { VisibilityFilters } from '../../constants';
+
+const { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } = VisibilityFilters;
 
 const Footer = () => {
   <p>
     Show:
     {" "}
-    <FilterLink filter="SHOW_ALL">All</FilterLink>
-    <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>
-    <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+    <FilterLink filter={ SHOW_ALL }>All</FilterLink>
+    {", "}
+    <FilterLink filter={ SHOW_ACTIVE }>Active</FilterLink>
+    {", "}
+    <FilterLink filter={ SHOW_COMPLETED }>Completed</FilterLink>
   </p>
 };
 
