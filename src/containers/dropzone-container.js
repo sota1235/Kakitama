684aa9c64eeb23f7c0bfadb2c4ea0efbffc6a6f6
sota1235 <sota1235@gmail.com>
2016-05-07 15:18:57 +0900

    modify: output music metadata when drag and dropping music file

--- src/containers/dropzone-container.js
+++ src/containers/dropzone-container.js
@@ -2,13 +2,22 @@
  * @description Container for dropping file action.
  */
 
+import fs from 'fs';
+import musicmetadata from 'musicmetadata';
 import React from 'react';
 import { connect } from 'react-redux';
 import Dropzone from 'react-dropzone';
+import { addMusic } from '../actions/actions';
 
 let MusicDropzone = ({ dispatch }) => {
   const onDrop = (files) => {
     console.log('Received files: ', files);
+    for (const index in files) {
+      let fname = files[index].path;
+      musicmetadata(fs.createReadStream(fname), (err, metadata) => {
+        console.log(metadata);
+      });
+    }
   };
 
   return (
