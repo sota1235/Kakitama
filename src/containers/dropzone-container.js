cd5c658ecf805cb3682fb378ef177c57dc60e6c9
sota1235 <sota1235@gmail.com>
2016-05-08 12:39:56 +0900

    modify: remove service logic

--- src/containers/dropzone-container.js
+++ src/containers/dropzone-container.js
@@ -2,22 +2,14 @@
  * @description Container for dropping file action.
  */
 
-import fs from 'fs';
-import musicmetadata from 'musicmetadata';
 import React from 'react';
 import { connect } from 'react-redux';
 import Dropzone from 'react-dropzone';
-import { addMusic } from '../actions/actions';
+import { uploadMusics } from '../actions/actions';
 
 let MusicDropzone = ({ dispatch }) => {
   const onDrop = (files) => {
-    console.log('Received files: ', files);
-    for (const index in files) {
-      let fname = files[index].path;
-      musicmetadata(fs.createReadStream(fname), (err, metadata) => {
-        console.log(metadata);
-      });
-    }
+    dispatch(uploadMusics(files));
   };
 
   return (
