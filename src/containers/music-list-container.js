0764bc730e1064947a133dd1a1a150319c5b007c
sota1235 <sota1235@gmail.com>
2016-05-08 12:43:50 +0900

    modify: fix to get data from store

--- src/containers/music-list-container.js
+++ src/containers/music-list-container.js
@@ -6,24 +6,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import MusicItemList from '../components/music-item-list-component';
 
-const mapStateToProps = (state, ownProps) => (
-  {
-    songs: [
-      {
-        id: 1,
-        songName: 'もっと',
-        albumName: 'もっと',
-        artistName: 'aiko',
-      },
-      {
-        id: 2,
-        songName: 'カブトムシ',
-        albumName: '桜の木の下',
-        artistName: 'aiko',
-      },
-    ]
-  }
-);
+const mapStateToProps = (state, ownProps) => ({ songs: state.musics });
 
 const MusicList = connect(mapStateToProps)(MusicItemList);
 
