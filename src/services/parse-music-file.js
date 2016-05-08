59777531353385125a7bf1c39ef68b0b8cb57b4b
sota1235 <sota1235@gmail.com>
2016-05-08 12:22:04 +0900

    add: module for parsing music file's meta data

---
+++ src/services/parse-music-file.js
@@ -0,0 +1,38 @@
+/**
+ * @description Parse music file and get meta data of it.
+ */
+
+import 'babel-polyfill';
+import fs from 'fs';
+import musicmetadata from 'musicmetadata';
+
+/**
+ * @description Wrapper for musicmetadata.
+ * @param {string} fileName
+ * @return {Promise}
+ */
+const getMusicMetaData = fileName =>
+  new Promise((resolve, reject) => {
+    musicmetadata(fs.createReadStream(fileName), (err, metadata) => {
+      if (err) {
+        reject(err);
+      }
+      resolve({
+        songName: metadata.title,
+        albumName: metadata.album,
+        artistName: metadata.artist[0],
+      });
+    });
+  });
+
+/**
+ * @description Parse music file.
+ * @param {Array} files
+ * @return {Promise}
+ */
+const parseMusics = async files => {
+  const promises = files.map(file => getMusicMetaData(file.path));
+  return await Promise.all(promises);
+};
+
+export default parseMusics;
