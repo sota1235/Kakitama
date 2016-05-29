6b14459613fd6d026787d3c6bc0715d15dea36fd
sota1235 <sota1235@gmail.com>
2016-05-29 14:42:48 +0900

    add: class file for playing audio on browser

---
+++ src/utils/audio.js
@@ -0,0 +1,82 @@
+/**
+ * @description Simple audio player by using Web Audio API.
+ */
+
+import 'babel-polyfill';
+import { isNull }        from 'lodash';
+import { readFile }      from './fs-promise';
+import { toArrayBuffer } from './buffer';
+
+/**
+ * @class AudioPlayer
+ * @description Util audio player for browser.
+ */
+export default class AudioPlayer {
+  constructor() {
+    // get native audio instance
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.context = new AudioContext;
+    this.source  = null;
+  }
+
+  /**
+   * @description Get blob data as a typed array of the music.
+   * @param {string} filePath
+   * @return {Promise}
+   * @private
+   */
+  async _getArrayBuffer(filePath) {
+    try {
+      const data = await readFile(filePath);
+      return await this._decodeAudioData(toArrayBuffer(data));
+    } catch (e) {
+      return null;
+    }
+  }
+
+  /**
+   * @description Decode audio data from array buffer.
+   * @param {ArrayBuffer} buffer
+   * @return {Promise}
+   */
+  async _decodeAudioData(buffer) {
+    return await this.context.decodeAudioData(buffer);
+  }
+
+  /**
+   * @description Play the music.
+   * @param {string} musicFilePath
+   */
+  async play(musicFilePath) {
+    this.source = this.context.createBufferSource();
+    this.source.buffer = await this._getArrayBuffer(musicFilePath);
+    this.source.connect(this.context.destination);
+    this.source.start(0);
+  }
+
+  /**
+   * @description Stop the music.
+   */
+  stop() {
+    if (!isNull(this.source)) {
+      this.source.stop();
+    }
+  }
+
+  /**
+   * @description Restart the music.
+   */
+  restart() {
+    if (!isNull(this.source)) {
+      this.source.start(0);
+    }
+  }
+
+  /**
+   * @description Change the volume of the music.
+   * @param {number} range
+   */
+  volume(range) {
+    //
+  }
+}
