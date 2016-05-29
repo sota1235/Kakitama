d873ac828a7d382ab6cb2c3b4dbaf32f1be353ca
sota1235 <sota1235@gmail.com>
2016-05-29 14:32:14 +0900

    add: helper for translate byte data

---
+++ src/utils/buffer.js
@@ -0,0 +1,15 @@
+/**
+ * @description Util functions for buffer.
+ */
+
+/**
+ * @description Convert buffer to ArrayBuffer.
+ */
+export function toArrayBuffer(buffer) {
+  const arrayBuffer = new ArrayBuffer(buffer.length);
+  const view        = new Uint8Array(arrayBuffer);
+  for (let i=0;i<buffer.length;++i) {
+    view[i] = buffer[i];
+  }
+  return arrayBuffer;
+};
