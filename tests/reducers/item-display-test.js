9fcbdfbe9e2535881e7998667f808390e9dd4d78
sota1235 <sota1235@gmail.com>
2016-07-17 16:22:28 +0900

    modify: setting for unit testing

---
+++ tests/reducers/item-display-test.js
@@ -0,0 +1,12 @@
+/**
+ * @description Unit tests for src/reducers/item-display.js
+ */
+
+import assert from 'assert';
+
+describe('reducers/item-display.js test', () => {
+  it('Sample', () => {
+    assert.equal(1, 1);
+  });
+});
+
