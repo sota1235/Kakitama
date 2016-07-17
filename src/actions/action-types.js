12b5fb765efd985de9f9f8cf4e3f11bad2c742d0
sota1235 <sota1235@gmail.com>
2016-07-17 18:26:42 +0900

    modify: add new action names

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -19,3 +19,21 @@ export const UPLOAD_MUSIC = 'UPLOAD_MUSIC';
  * @type {string}
  */
 export const CLICK_MUSIC_ITEM = 'CLICK_MUSIC_ITEM';
+
+/**
+ * @description Start music.
+ * @type {string}
+ */
+export const START_PLAYING_MUSIC = 'START_PLAYING_MUSIC';
+
+/**
+ * @description Change to prev music.
+ * @type {string}
+ */
+export const PREV_MUSIC = 'CHANGE_MUSIC_TO_PREV';
+
+/**
+ * @description Change to next music.
+ * @type {string}
+ */
+export const NEXT_MUSIC = 'CHANGE_MUSIC_TO_NEXT';
