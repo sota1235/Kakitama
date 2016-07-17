89b00450705ab8472162c06b24b4191ee85b1d64
sota1235 <sota1235@gmail.com>
2016-07-17 16:33:09 +0900

    add: unit test for reducer/item-display

--- tests/reducers/item-display-test.js
+++ tests/reducers/item-display-test.js
@@ -2,11 +2,48 @@
  * @description Unit tests for src/reducers/item-display.js
  */
 
-import assert from 'assert';
+import assert     from 'assert';
+import reducer    from 'reducers/item-display';
+import * as types from 'actions/action-types';
 
 describe('reducers/item-display.js test', () => {
-  it('Sample', () => {
-    assert.equal(1, 1);
+  it('Should return default state.', () => {
+    const expected = [];
+
+    assert.equal(expected, reducer(expected, {}));
+  });
+
+  it('Should handle CLICK_MUSIC_ITEM', () => {
+    const action = {
+      type     : types.CLICK_MUSIC_ITEM,
+      id       : 1235,
+      songName : 'song-name',
+    };
+    const expected = {
+      id       : action.id,
+      songName : action.songName,
+    };
+
+    assert.deepEqual(expected, reducer([], action));
+  });
+
+  it('Should override state when CLICK_MUSIC_ITEM', () => {
+    const state = [
+      {
+        id       : 1236,
+        songName : 'another-song-name',
+      }
+    ];
+    const action = {
+      type     : types.CLICK_MUSIC_ITEM,
+      id       : 1235,
+      songName : 'song-name',
+    };
+    const expected = {
+      id       : action.id,
+      songName : action.songName,
+    };
+
+    assert.deepEqual(expected, reducer(state, action));
   });
 });
-
