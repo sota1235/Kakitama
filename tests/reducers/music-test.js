3b2b5278b1b0eb419bc5f1f236dbbcd89974daec
sota1235 <sota1235@gmail.com>
2016-07-17 16:44:56 +0900

    modify: add test cases for reducers/music-test

--- tests/reducers/music-test.js
+++ tests/reducers/music-test.js
@@ -12,4 +12,62 @@ describe('reducers/music.js test', () => {
 
     assert.deepEqual(expected, reducer(undefined, {}));
   });
+
+  it('Should threw action', () => {
+    const state = [
+      'song-data1', 'song-data2',
+    ];
+
+    assert.deepEqual(state, reducer(state, {}));
+  });
+
+  it('Should add musics(State is still empty or undefined).', () => {
+    const action = {
+      type       : types.ADD_MUSIC,
+      id         : 1235,
+      songName   : 'song-name',
+      albumName  : 'album-name',
+      artistName : 'artist-name',
+      filePath   : 'file-path',
+    };
+    const expected = [
+      {
+        id         : action.id,
+        songName   : action.songName,
+        albumName  : action.albumName,
+        artistName : action.artistName,
+        filePath   : action.filePath,
+      },
+    ];
+
+    assert.deepEqual(expected, reducer(undefined, action));
+    assert.deepEqual(expected, reducer([], action));
+  });
+
+  it('Should add musics(some musics are already added to state).', () => {
+    const state = [
+      'song-data-1',
+      'song-data-2',
+    ];
+    const action = {
+      type       : types.ADD_MUSIC,
+      id         : 1235,
+      songName   : 'song-name',
+      albumName  : 'album-name',
+      artistName : 'artist-name',
+      filePath   : 'file-path',
+    };
+    const expected = [
+      ...state,
+      {
+        id         : action.id,
+        songName   : action.songName,
+        albumName  : action.albumName,
+        artistName : action.artistName,
+        filePath   : action.filePath,
+      },
+    ];
+
+    assert.deepEqual(expected, reducer(state, action));
+  });
 });
